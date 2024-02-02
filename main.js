1b61105c10a76abc4d68535872d2a1778f087a52
Andres M. Prieto <andresprieto2703@gmail.com>
2024-02-02 18:59:12 -0500

    Using a better approach to the server

--- main.js
+++ main.js
@@ -1,25 +1,22 @@
-const cors = require('cors');
-const express = require('express');
-const routerApi = require('./routes');
+const cors = require("cors");
+const express = require("express");
+const routerApi = require("./routes");
 
+const HOST = process.env.HOST || "localhost";
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 // This middleware is used to handle JSON
 app.use(express.json());
 
-const whiteList = [
-  'http://localhost:8080',
-  'https://example.com',
-  'https://myapp.co',
-];
+const whiteList = [`http://${HOST}:${PORT}`];
 
 const origins = {
   origin: (origin, callback) => {
     if (whiteList.includes(origin) || !origin) {
       callback(null, true);
     } else {
-      callback(new Error('Access denied'));
+      callback(new Error("Access denied"));
     }
   },
 };
@@ -28,6 +25,4 @@ app.use(cors(origins));
 
 routerApi(app);
 
-app.listen(PORT, () => {
-  console.log(`App is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
